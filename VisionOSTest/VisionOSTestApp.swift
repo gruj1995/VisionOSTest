3bc33b97f425f0a7a96ef7a380e9bfcb58c49c69
developer <gruj1995@berich.net.tw>
2023-07-03 16:09:33 +0800

    feat: init

---
+++ VisionOSTest/VisionOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSTestApp.swift
+//  VisionOSTest
+//
+//  Created by 李品毅 on 2023/7/3.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
